9147913c03056df77d11baa82103e267c3641160
Chris Martello <chrismartello@gmail.com>
2021-03-08 20:40:01 -0600

    Initial commit.  Drawing, translation, and scaling implemented.

---
+++ shape2d.py
@@ -0,0 +1,80 @@
+import pygame
+
+class shape2d:
+    def __init__(self, source):
+        self.source = source
+        self.verticies = []
+        self.lines = []
+        self.load2d(self.source)
+        self.tx = 0
+        self.ty = 0
+
+    def load2d(self, source):
+        """Loads a shape file which contains a series of verticies and lines.
+        Lines starting with a v are verticies and are defined as x/y coordinates.
+        Lines starting with an h are lines and are a pair of verticies to draw
+        lines between.  This is a stupid and brittle parser right now."""
+
+        fh = open(self.source)
+        for line in fh.readlines():
+            if line[0] == '#':
+                next
+            if line[0] == 'v':
+                self.verticies.append((int(line.split(':')[1]), int(line.split(':')[2])))
+            if line[0] == 'l':
+                self.lines.append((int(line.split(':')[1]), int(line.split(':')[2])))
+
+
+    def draw(self, surface):
+        """Accepts a pygame surface and draws the shape to that surface."""
+
+        def origin_convert(vertex, surf_size):
+            """Only needed in the draw function, moves the shape to the
+            center of the screen."""
+
+            width, height = surf_size
+            x,y = vertex[0], vertex[1]
+            x1 = x + width / 2
+            y1 = -y + height / 2
+
+            return (x1, y1)
+
+        surf_sz = surface.get_size()
+    
+        for entry in self.lines:
+            # put the shape at the center of the screen
+            start = origin_convert(self.verticies[entry[0]], surf_sz)
+            end = origin_convert(self.verticies[entry[1]], surf_sz)
+
+            # add translation offset
+            start = (start[0] + self.tx, start[1] + self.ty)
+            end = (end[0] + self.tx, end[1] + self.ty)
+        
+            # and print
+            pygame.draw.line(surface, (0, 255, 0), start, end)
+
+
+    def translate(self, dx, dy):
+        """Accepts a delta x and y value and modifies tx and ty which store
+        the offset to draw the shape at.  These are referenced by .draw()
+        instead of changing the verticies since that would make rotation
+        stop working correctly."""
+        self.tx += dx
+        self.ty += dy
+
+    def scale(self, factor):
+        """Shrinks or enlarges the shape based on factor.  Values lower than
+        1.0 shrink it and greater than 1.0 grow it.  1 is a waste of time.
+        Because scaling doesn't change the local origin, it is safe to apply
+        the scaling factor to the verticies."""
+
+        # nyah
+        if factor == 1:
+            return
+
+        # unfortunatly, we're working with tuples, so we'll have to replace
+        # them instead of changing their value in place. :(
+        for vert in range(len(self.verticies)):
+            x, y = self.verticies[vert]
+            self.verticies[vert] = (x * factor, y * factor)
+            
---
+++ test.py
@@ -0,0 +1,66 @@
+import pygame
+
+from pygame.locals import *
+
+from shape2d import shape2d
+
+# boot
+pygame.init()
+pygame.font.init()
+
+# for throttling and tracking FPS
+fps = pygame.time.Clock()
+
+FPS_LIMIT = 60
+
+# we'll use SCREEN_WIDTH and SCREEN_HEIGHT for coordinate translation 
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+
+# pygame needs a tuple for creating its window, so pack those up
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+
+# create screen
+screen = pygame.display.set_mode(SCREEN_SIZE)
+
+# load the shape we want to draw
+shape = shape2d('square.2')
+
+# core loop
+QUIT = False
+
+def origin_convert(vertex):
+    x, y = vertex[0], vertex[1]
+    x1 =  x + SCREEN_WIDTH / 2
+    y1 = -y + SCREEN_HEIGHT / 2
+
+    return (x1, y1)
+
+while not QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.mod == pygame.KMOD_NONE:
+                pass
+            # all of our key bindings go here
+            if event.key == pygame.K_ESCAPE:
+                QUIT = True
+            # WASD for shape translation
+            if event.key == pygame.K_w:
+                shape.translate(0, -5)
+            if event.key == pygame.K_a:
+                shape.translate(-5, 0)
+            if event.key == pygame.K_s:
+                shape.translate(0, 5)
+            if event.key == pygame.K_d:
+                shape.translate(5, 0)
+            if event.key == pygame.K_z:
+                shape.scale(0.95)
+            if event.key == pygame.K_c:
+                shape.scale(1.05)
+
+    screen.fill(pygame.Color(0,0,0))
+
+    # draw the loaded shape
+    shape.draw(screen)
+
+    pygame.display.update()
+    fps.tick(FPS_LIMIT)
